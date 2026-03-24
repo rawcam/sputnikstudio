5690016142b96026e966ede70c34888bba0b17ab
rawcam <rawcam@ya.ru>
2026-03-24 15:33:13 +0300

    Update sound.js

--- js/sound.js
+++ js/sound.js
@@ -1,1 +1,7 @@
-
+// sound.js
+const LedModule = (function() {
+    let unsubscribe = null;
+    function init() { console.log('LedModule stub'); unsubscribe = AppState.subscribe(()=>{}); }
+    function destroy() { if (unsubscribe) unsubscribe(); }
+    return { init, destroy };
+})();
