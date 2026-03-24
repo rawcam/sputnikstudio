d595386ca1851c434e9088fe598177b9334bc42c
rawcam <rawcam@ya.ru>
2026-03-24 15:34:22 +0300

    Update ergo.js

--- js/ergo.js
+++ js/ergo.js
@@ -1,1 +1,7 @@
-
+// led.js
+const LedModule = (function() {
+    let unsubscribe = null;
+    function init() { console.log('LedModule stub'); unsubscribe = AppState.subscribe(()=>{}); }
+    function destroy() { if (unsubscribe) unsubscribe(); }
+    return { init, destroy };
+})();
